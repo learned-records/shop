2907212b8847d1f9b7ea4dbe1fd73e2ecaf82e8a
choi-ys <project.log.062@gmail.com>
2024-07-31 18:53:52 +0900

    feat: naver 상품 검색 api 구현

---
+++ src/main/java/io/sparta/shop/naver/controller/NaverApiController.java
@@ -0,0 +1,23 @@
+package io.sparta.shop.naver.controller;
+
+import io.sparta.shop.naver.dto.ItemDto;
+import io.sparta.shop.naver.service.NaverApiService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class NaverApiController {
+
+    private final NaverApiService naverApiService;
+
+    @GetMapping("/search")
+    public List<ItemDto> searchItems(@RequestParam String query) {
+        return naverApiService.searchItems(query);
+    }
+}
